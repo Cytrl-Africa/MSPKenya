e80a8f29751c570e9edc2942008ab8ce26bf4d93
Collinsturi <collinsturi@gmail.com>
2026-05-11 12:31:17 +0300

    Fix: Moved the query provider to the root folder and also separated the query provider with the query client

---
+++ providers/query-provider.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { queryClient } from "@/lib/api/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode} from "react"
+
+export default function QueryProvider({
+    children,
+}: {
+    children: ReactNode;
+}){
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
+}
